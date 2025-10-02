03c9801ca3dd72c117dffd378bdd585bf7c287bc
Diki Saputra <diki@CNCRD.LAN>
2025-10-02 10:11:48 +0700

    delete flight information form

--- resources/views/pages/master/editairport.blade.php
+++ resources/views/pages/master/editairport.blade.php
@@ -776,24 +776,24 @@
           </div>
         </div>
 
-         <div class="col-md-12">
+         <!-- <div class="col-md-12">
           <div class="card card-outline card-info">
             <div class="card-header">
               <h3 class="card-title">
                 Edit Flight Information
               </h3>
             </div>
-            <!-- /.card-header -->
+
             <div class="card-body">
 
                 <textarea id="summernote15" name="flight_information">
-                    <?php echo $airport->flight_information; ?>
+
                 </textarea>
 
             </div>
 
           </div>
-        </div>
+        </div> -->
 
         <div class="col-md-12">
           <div class="card card-outline card-info">
